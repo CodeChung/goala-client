aab2cc25ad310e678e60544469f84d534de3c35c
Harry Chung <hc9825@gmail.com>
2019-09-19 23:57:18 -0700

    added tests and css for sidebar

--- src/components/SideBar/SideBar.js
+++ src/components/SideBar/SideBar.js
@@ -2,49 +2,85 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import './SideBar.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faCalendar, faCheckSquare, faStar, faBell, faSignOutAlt } from '@fortawesome/free-solid-svg-icons';
+import { faHome, faCalendar, faCheckSquare, faStar, faBell, faSearch, faSignOutAlt } from '@fortawesome/free-solid-svg-icons';
 
 class SideBar extends React.Component {
     state = {
-        current: ''
+        currentView: 'home'
+    }
+    setCurrentNav(currentView) {
+        console.log(currentView)
+        this.setState({ currentView })
     }
     render() {
+        const { currentView } = this.state
         return (
             <nav className='sidebar'>
-                <Link to='/'>
+                <Link to='/'
+                    onClick={() => this.setCurrentNav('home')}
+                    className='nav-logo'>
                     <span className='logo'>
-                        Goalf
+                    <img src='https://image.flaticon.com/icons/svg/141/141742.svg' alt='logo Icon made by Freepik from www.flaticon.com' />
+                        Goala
                     </span>
                 </Link>
-                <Link to='/calendar'>
+                <Link to='/'
+                    onClick={() => this.setCurrentNav('home')}
+                    className={ currentView === 'home' ? 'nav-active' : undefined }>
+                    <div className='sidebar-icon'>
+                        <FontAwesomeIcon 
+                            icon={faHome} /> 
+                    </div>
+                    Home
+                </Link>
+                <Link to='/calendar'
+                    onClick={() => this.setCurrentNav('calendar')}
+                    className={ currentView === 'calendar' ? 'nav-active' : undefined }>
                     <div className='sidebar-icon'>
                         <FontAwesomeIcon 
                             icon={faCalendar} /> 
                     </div>
                     Calendar
                 </Link>
-                <Link to='/actions'>
+                <Link to='/actions'
+                    onClick={() => this.setCurrentNav('actions')}
+                    className={ currentView === 'actions' ? 'nav-active' : undefined }>
                     <div className='sidebar-icon'>
                         <FontAwesomeIcon 
                             icon={faCheckSquare} /> 
                     </div>
                     Actions
                 </Link>
-                <Link to='/reminders'>
+                <Link to='/reminders'
+                    onClick={() => this.setCurrentNav('reminders')}
+                    className={ currentView === 'reminders' ? 'nav-active' : undefined }>
                     <div className='sidebar-icon'>
                         <FontAwesomeIcon 
                             icon={faBell} /> 
                     </div>
                     Reminders
                 </Link>
-                <Link to='/saved'>
+                <Link to='/saved'
+                    onClick={() => this.setCurrentNav('saved')}
+                    className={ currentView === 'saved' ? 'nav-active' : undefined }>
                     <div className='sidebar-icon'>
                         <FontAwesomeIcon 
                             icon={faStar} /> 
                     </div>
                     Saved
                 </Link>
-                <Link to='/login' onClick={() => this.props.logout()}>
+                <Link to='/search'
+                    onClick={() => this.setCurrentNav('search')}
+                    className={ currentView === 'search' ? 'nav-active' : undefined }>
+                    <div className='sidebar-icon'>
+                        <FontAwesomeIcon 
+                            icon={faSearch} /> 
+                    </div>
+                    Search
+                </Link>
+                <Link to='/login' 
+                    className='nav-end'
+                    onClick={() => this.props.logout()}>
                     <div className='sidebar-icon'>
                         <FontAwesomeIcon 
                             icon={faSignOutAlt} rotation={180} />
