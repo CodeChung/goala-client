cb75e23812a060f4677c5137b98c6fd659c4f327
Harry Chung <hc9825@gmail.com>
2019-09-22 17:51:59 -0700

    dragndrop multiple columns now working

--- src/components/ToolBox/ActionTools/ActionTools.js
+++ src/components/ToolBox/ActionTools/ActionTools.js
@@ -2,30 +2,131 @@ import React from 'react';
 import Column from './Column';
 import { DragDropContext } from 'react-beautiful-dnd';
 import './ActionTools.css';
+import styled from 'styled-components';
 
+// tasks.content is where I could store individual component blocks
 const initialData = {
     tasks: { 
-        'task-1': { id: 'task-1', content: 'Take out the garbage' },
-        'task-2': { id: 'task-2', content: 'Take out the garbage' },
-        'task-3': { id: 'task-3', content: 'Take out the garbage' },
-        'task-4': { id: 'task-4', content: 'Take out the garbage' },
+        'task-1': { id: 'task-1', content: 'Title' },
+        'task-2': { id: 'task-2', content: 'Eat' },
+        'task-3': { id: 'task-3', content: 'Go for a walk' },
+        'task-4': { id: 'task-4', content: 'Go running' },
+        'task-5': { id: 'task-5', content: 'Take out the garbage' },
+        'task-6': { id: 'task-6', content: 'Eat' },
+        'task-7': { id: 'task-7', content: 'Go for a walk' },
+        'task-8': { id: 'task-8', content: 'Go running' },
+        'task-9': { id: 'task-9', content: 'Take out the garbage' },
+        'task-10': { id: 'task-10', content: 'Eat' },
+        'task-11': { id: 'task-11', content: 'Go for a walk' },
+        'task-12': { id: 'task-12', content: 'Go running' },
     },
     columns: {
         'column-1': {
             id: 'column-1',
-            title: 'To do',
+            title: 'Action',
             taskIds: ['task-1', 'task-2', 'task-3', 'task-4']
+        },
+        'column-2': {
+            id: 'column-2',
+            title: 'Toolbox',
+            // taskIds: ['task-1', 'task-2', 'task-3', 'task-4']
+            taskIds: []
+        },
+        'column-3': {
+            id: 'column-3',
+            title: 'Done',
+            // taskIds: ['task-1', 'task-2', 'task-3', 'task-4']
+            taskIds: []
         }
     },
-    columnOrder: ['column-1']
+    columnOrder: ['column-1', 'column-2', 'column-3']
 }
+
+const Container = styled.div `
+    display: flex;
+`
 
 class ActionTools extends React.Component {
     state = initialData;
 
-    onDragEnd = result => {
-
+    onDragStart = () => {
+        document.body.style.color = 'orange';
     }
+
+    onDragpUpdate = update => {
+        const { destination } = update;
+        const opacity = destination
+            ? destination.index / Object.keys(this.state.tasks).length
+            : 0;
+        document.body.style.backgroundColor = `rgba(153, 141, 217, ${opacity})`;
+    }
+
+    onDragEnd = result => {
+        document.body.style.color = 'inherit';
+        document.body.style.backgroundColor = 'inherit'
+
+        const { destination, source, draggableId } = result;
+
+        if (!destination) {
+            return;
+        }
+
+        if (
+            destination.droppableId === source.droppableId &&
+            destination.index === source.index
+        ) {
+            return
+        }
+
+        const start = this.state.columns[source.droppableId];
+        const finish = this.state.columns[destination.droppableId];
+
+        if (start === finish) {
+            const newTaskIds = Array.from(start.taskIds);
+            newTaskIds.splice(source.index, 1);
+            newTaskIds.splice(destination.index, 0, draggableId)
+
+            const newColumn = {
+                ...this.state,
+                taskIds: newTaskIds,
+            }
+
+            const newState = {
+                ...this.state,
+                columns: {
+                    ...this.state.columns,
+                    [newColumn.id]: newColumn,
+                },
+            };
+            this.setState(newState)
+            return
+        }
+        
+        // Moving from one list to another
+        const startTaskIds = Array.from(start.taskIds);
+        startTaskIds.splice(source.index, 1);
+        const newStart = {
+            ...start,
+            taskIds: startTaskIds,
+        };
+
+        const finishTaskIds = Array.from(finish.taskIds);
+        finishTaskIds.splice(destination.index, 0, draggableId);
+        const newFinish = {
+            ...finish,
+            taskIds: finishTaskIds,
+        };
+        
+        const newState = {
+            ...this.state,
+            columns: {
+                ...this.state.columns,
+                [newStart.id]: newStart,
+                [newFinish.id]: newFinish,
+            },
+        };
+        this.setState(newState)
+    };
     render() {
         const columns = this.state.columnOrder.map(columnId => {
             const column = this.state.columns[columnId]
@@ -36,10 +137,13 @@ class ActionTools extends React.Component {
         return (
             <div className='action-tools'>
                 <DragDropContext
+                    onDragStart={this.onDragStart}
                     onDragEnd={this.onDragEnd}
-                    >
-                    <h4>Values</h4>
-                    {columns}
+                >
+                    <Container>
+                        <h4>Values</h4>
+                        {columns}
+                    </Container>
                 </DragDropContext>
                 
             </div>
